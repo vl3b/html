4a0a5cbe814b8c274b0176a35ffce68d81cb2fe1
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-18 21:12:24 +0200

    improvement

--- keeganuwu.php
+++ keeganuwu.php
@@ -1,13 +1,13 @@
-<html>
+<html style="background-color: #666666;">
 <head>
 </html>
 <body>
 <?php
-for ($x = 0; $x <= 10; $x++) {
+for ($x = 0; $x <= 30; $x++) {
 $number = strval(rand(1000,9999));
 $lettera = chr(rand(97,122));
 $letterb = chr(rand(97,122));
-echo("<a href='https://prnt.sc/$lettera$letterb$number'><p>click me! $lettera$letterb$number</p></a>");
+echo("<a href='https://prnt.sc/$lettera$letterb$number'><span>click me! $lettera$letterb$number</span></a><br>");
 }
 ?>
 </body>

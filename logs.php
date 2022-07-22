6b3dd9121ce536179ed28837bff4873b6251da55
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 13:54:09 +0200

    lo

--- logs.php
+++ logs.php
@@ -10,8 +10,8 @@
     $filepath = "$path/logs";
     $fileContent = file_get_contents($filepath);
     $contentArray = explode("startofscript", $fileContent);
+    echo $fileContent;
     foreach ($contentArray as $content) {
-        $content = trim($content, "tag");
         if (str_contains($content, 'error')) {
             echo ("<div style=background-color:red;>");
         } else {

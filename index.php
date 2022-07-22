ce83edc677a8e9b792702f962860fb1dc1a18e63
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 12:54:30 +0200

    better scripts

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     </form>
     <?php
     if (isset($_POST['openshellfile'])) {
-        echo $res = shell_exec('/var/www/html/manual_gitclone.sh');
+        echo $res = shell_exec('/var/www/html/shscripts/manual_gitclone.sh');
     }
     ?>
 </body>
--- shscripts/manual_gitclone.sh
+++ shscripts/manual_gitclone.sh
@@ -1,5 +1,5 @@
 cd /var/www/html
-printf "===============================gitclone.sh\n" >> /var/www/html/logs 2>&1
+printf "tag='git'tag='git clone'scriptname='manual_gitclone.sh'\n" >> /var/www/html/logs 2>&1
 time=$(date)
 printf "$time\n" >> /var/www/html/logs 2>&1
 git pull https://github.com/vl3b/html >> /var/www/html/logs 2>&1

fcbfc8643ef4334402091167cd748f650cc64d71
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 11:47:05 +0200

    test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <input type='submit' value='build from git' name='run.sh manual_gitclone.sh' />
     </form>
     <?php
-    if (isset($_POST['run manual_gitclone.sh'])) {
+    if (isset($_POST['run.sh manual_gitclone.sh'])) {
         echo($_POST);
         echo ("trueee");
         echo $res = shell_exec('/var/www/html/manual_gitclone.sh');

b2fa3ae2317037e85abcb7390accfc0228384bbd
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 13:01:51 +0200

    printf "startofscript" >> /var/www/html/logs 2>&1

--- shscripts/manual_gitclone.sh
+++ shscripts/manual_gitclone.sh
@@ -1,4 +1,5 @@
 cd /var/www/html
+printf "startofscript" >> /var/www/html/logs 2>&1
 printf "tag='git'tag='git clone'scriptname='manual_gitclone.sh'\n" >> /var/www/html/logs 2>&1
 time=$(date)
 printf "$time\n" >> /var/www/html/logs 2>&1

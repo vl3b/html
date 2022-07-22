a88eec18f920667042b1ab44a8cc770ae042ec06
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 11:46:33 +0200

    test

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 
 <body>
     <form action='' method='post'>
-        <input type='submit' value='gaming' name='openshellfile' />
+        <input type='submit' value='build from git' name='run.sh manual_gitclone.sh' />
     </form>
     <?php
-    if (isset($_POST['openshellfile'])) {
+    if (isset($_POST['run manual_gitclone.sh'])) {
+        echo($_POST);
         echo ("trueee");
         echo $res = shell_exec('/var/www/html/manual_gitclone.sh');
     }

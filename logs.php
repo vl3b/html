02e233db1f69d96e10f0431139ee197c4e55c7c1
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 13:51:21 +0200

    updated logs

--- logs.php
+++ logs.php
@@ -5,21 +5,20 @@
 
 <body>
     <?php
-        include 'header.html';
-        $path = getcwd();
-        $filepath = "$path/logs";
-        $fileContent = file_get_contents($filepath);
-        echo $filepath;
-        echo $fileContent;
-        $contentArray = explode("startofscript", $fileContent);
-        foreach ($contentArray as $content){
-            if (str_contains($content,'=') && str_contains($content,'gitclone.sh')){
-                echo ("<br>");
-                echo ("<span>$content</span>");
-            } else {
-                echo ("<span>$content</span>");
-            }
+    include 'header.html';
+    $path = getcwd();
+    $filepath = "$path/logs";
+    $fileContent = file_get_contents($filepath);
+    $contentArray = explode("startofscript", $fileContent);
+    foreach ($contentArray as $content) {
+        $content = trim($content, "tag");
+        if (str_contains($content, 'error')) {
+            echo ("<div style=background-color:red;>");
+        } else {
+            echo ("<div>");
         }
+        echo ("<span>$content</span></div>");
+    }
     ?>
 </body>
 

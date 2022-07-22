7506f5a0f9a5de22531e59b95fc6f64c982484bf
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 10:59:08 +0200

    execute

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <html>
+
 <head>
     <title>Title!</title>
 </head>
+
 <body>
+    <form action='shell.php' method='post'>
+        <input type='submit' value='gaming' name='openshellfile' />
+    </form>
     <?php
-    shell_exec('sh /var/www/html/test.sh');
+    if (isset($_POST['openshellfile'])) {
+        echo $res = shell_exec('/var/www/html/test.sh');
+    }
     ?>
     <p>test7</p>
 </body>
+
 </html>

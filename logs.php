a483885f3b67eaf9acdfc79c70d636e943bee4d6
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 14:01:27 +0200

    test

--- logs.php
+++ logs.php
@@ -10,7 +10,6 @@
     $filepath = "$path/logs";
     $fileContent = file_get_contents($filepath);
     $contentArray = explode('startofscript', $fileContent);
-    echo $fileContent;
     foreach ($contentArray as $content) {
         if (strpos($content, 'error', 0)) {
             echo ("<div style=background-color:red;>");

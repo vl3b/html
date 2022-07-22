d3914782baab26eaf65adda75062ae46ed35300e
vl3b <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-22 11:51:14 +0200

    asd

--- index.php
+++ index.php
@@ -7,11 +7,15 @@
 <body>
     <form action='' method='post'>
         <input type='submit' value='build from git' name='openshellfile' />
+        <input type='submit' value='test button' name='test' />
     </form>
     <?php
     if (isset($_POST['openshellfile'])) {
         echo ("trueee");
         echo $res = shell_exec('/var/www/html/manual_gitclone.sh');
+    }
+    if (isset($_POST['test'])) {
+        echo $_POST;
     }
     ?>
     <p>tet85</p>

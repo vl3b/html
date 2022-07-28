fe952cccbd049c961fd9f5761177c6cf44378262
tobiasgjerstrup <58333752+tobiasgjerstrup@users.noreply.github.com>
2022-07-28 13:23:43 +0200

    removed default gitcheck from logs

--- logs.php
+++ logs.php
@@ -11,12 +11,14 @@
     $fileContent = file_get_contents($filepath);
     $contentArray = explode('startofscript', $fileContent);
     foreach (array_reverse($contentArray) as $content) {
-        if (strpos($content, 'error', 0) or strpos($content, 'fatal', 0)) {
-            echo ("<div style=background-color:red;>");
-        } else {
-            echo ("<div>");
+        if (strpos($content, 'Already up to date.', 0){
+            if (strpos($content, 'error', 0) or strpos($content, 'fatal', 0)) {
+                echo ("<div style=background-color:red;>");
+            } else {
+                echo ("<div>");
+            }
+            echo ("<span>$content</span></div>");
         }
-        echo ("<span>$content</span></div>");
     }
     ?>
 </body>
